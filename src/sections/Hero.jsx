003746559fb804599b2d8dc949d8c7784ea95967
Adrian Cheba <adiche@wp.pl>
2026-01-12 18:04:14 +0100

    Added animated button to Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,7 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "../components/Button";
+import AnimatedButton from "../components/AnimatedButton";
+
 const Hero = () => {
   return (
     <section className="relative min-h-screen items-center overflow-hidden">
@@ -25,6 +29,42 @@ const Hero = () => {
             }}
           />
         ))}
+      </div>
+      <div className="container mx-auto px-6 pt-32 pb-20 relative z-10">
+        <div>
+          <div className="space-y-8">
+            <div className="animate-fade-in">
+              <span className=" inline-flex items-center gap-2 px-4 py-2 glass text-sm text-primary rounded-full">
+                <span className="w-2 h-2 bg-primary rounded-full animate-pulse" />
+                Lorem ipsum
+              </span>
+            </div>
+
+            <div className="space-y-4">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold leading-tight animate-fade-in animation-delay-100">
+                Lorem <span className="text-primary glow-text"> ipsum </span>
+                <br />
+                lorem ipsum
+                <br />
+                <span className="font-serif italic font-normal text-white">
+                  lorem
+                </span>
+              </h1>
+              <p className=" text-lg text-mute-foreground max-w-lg animate-fade-in animation-delay-200">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div className=" flex flex-wrap gap-4 animate-fade-in animation-delay-300">
+              <Button size="lg">
+                Contact Me <ArrowRight className="w-5 h-5" />
+              </Button>
+              <AnimatedButton />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
